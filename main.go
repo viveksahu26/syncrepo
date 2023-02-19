e80b9332af0feb41da11b08e335b086dc0bd8dcf
Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
2023-02-19 20:07:18 +0530

    added runner function
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>

--- main.go
+++ main.go
@@ -5,12 +5,18 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/viveksahu26/syncrepo/config"
+	"github.com/viveksahu26/syncrepo/pkg/git"
+	"github.com/viveksahu26/syncrepo/pkg/http"
+	"github.com/viveksahu26/syncrepo/pkg/syncrepo"
 )
 
 func main() {
 	// Initialize configuration values like PORT
 	// DEBUG, LOG, SYNC_REPO_PATH
 	config.Init()
+
+	// initialize log
+	//
 
 	// Initialize confidentials values like tokens, password, etc
 	config.ConfidentialInit()
@@ -19,12 +25,25 @@ func main() {
 	echoServer := echo.New()
 
 	// get the port
-	port := config.TempConfigFile.Server.Port
+	port := config.GetServerConfig().Port
 
 	// errChan := make(chan error)
 
+	// initialize git service
+	gitService := git.InitGitServices()
+
+	// init sync repo service
+	syncRepoService := syncrepo.InitSyncRepoServices(gitService)
+
+	// init handlers
+	http.InitSyncRepoHttpHandler(echoServer, syncRepoService)
+
 	// server is started on port 8000
-	echoServer.Logger.Fatal(echoServer.Start(":" + strconv.Itoa(port)))
+	// echoServer.Logger.Fatal(echoServer.Start(":" + strconv.Itoa(port)))
+
+	errChan := make(chan error)
+
+	go runner(echoServer, port, errChan)
 
 	/*
 		Go’s `net/http` package defines a handler function signature as a f
@@ -33,4 +52,12 @@ func main() {
 		The handler type in Echo is a function that takes in an Echo “Context”,
 		and returns an error.
 	*/
+}
+
+func runner(e *echo.Echo, port int, errChan chan error) {
+	err := e.Start(":" + strconv.Itoa(port))
+	// return errors.Wrap(err, "error listening on port err "+strconv.Itoa(port))
+	if err != nil {
+		errChan <- err
+	}
 }
